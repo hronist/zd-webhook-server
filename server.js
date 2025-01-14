a48bd3997a4655318bb445b2db5941e90889e061
Makar <hronist@gmail.com>
2025-01-14 14:48:45 +0300

    change rule

--- server.js
+++ server.js
@@ -8,11 +8,14 @@ app.use(express.json());
 app.post('/webhook', (req, res) => {
   const { userOp, chainId, projectId } = req.body;
   console.log('Webhook data received:', userOp, chainId, projectId);
+  if (userOp.toString().includes('1231deb6f5749ef6ce6943a275a1d3e7486f4eae')){
+    res.json({ proceed: true });   
+  }
 
   // Add some logic to determine if the userop should be sponsored
 
   // If the userOp should be sponsored, return proceed: true. Else, return proceed: false
-  res.json({ proceed: true }); 
+  res.json({ proceed: false }); 
 });
 
 app.listen(PORT, () => {
